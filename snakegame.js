9911cc91686e46ff20ba1e0406d782ffd67a1137
Shubhankar Singh <Shubhankar.singh@outlook.com>
2020-03-09 17:31:34 -0500

    Update snakegame.js

--- snakegame.js
+++ snakegame.js
@@ -24,7 +24,6 @@ var apple = {
 };
 
 // get random whole numbers in a specific range
-// @see https://stackoverflow.com/a/1527820/2124254
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
